ab73bb93574e1f303073e21252c08ff80f88661c
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2025-03-24 17:53:49 -0300

    buscando el error4

--- student-frontend/src/views/welcome.jsx
+++ student-frontend/src/views/welcome.jsx
@@ -3,23 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import welcomeImage from '../assets/welcome.png'; // Importa la imagen
 
 export const Welcome = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="container d-flex flex-column align-items-center justify-content-center vh-100 text-center">
-      <img 
-        src={welcomeImage} 
-        alt="Bienvenido" 
-        className="img-fluid mb-4" 
-        style={{ maxWidth: '300px' }} 
-      />
-      <h1 className="mb-3">¡Bienvenido a UniTrack!</h1>
-      <p className="lead mb-4">
-        UniTrack es una aplicación de seguimiento en tiempo real diseñada para ayudarte a llegar a tiempo a tu lugar de estudio. ¡Esperamos que te sea de gran utilidad!
-      </p>
       <button 
         className="btn btn-primary btn-lg" 
-        onClick={() => navigate('/filtrarParada')}
+        onClick={() => alert('Navegación deshabilitada temporalmente')}
       >
         Ingresar
       </button>
