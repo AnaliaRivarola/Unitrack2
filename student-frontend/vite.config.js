e802e71837a29ea054ebb93daa283e95820da232
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2025-03-24 17:02:28 -0300

    pruebando otra vez2

--- student-frontend/vite.config.js
+++ student-frontend/vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "./public/icons/logo.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "./public/icons/logo.png",
             sizes: "512x512",
             type: "image/png",
           },
