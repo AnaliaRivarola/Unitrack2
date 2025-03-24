3fad50fc011be6f49d138d0f2428176ca03b33f4
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2025-03-24 17:49:32 -0300

    buscando el error3

--- student-frontend/src/App.jsx
+++ student-frontend/src/App.jsx
@@ -2,25 +2,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    axios.get('https://unitrack2.onrender.com/Unitruck') // Cambia esto si tu ruta de API es diferente
-      .then(response => {
-        console.log('Datos obtenidos:', response.data);
-      })
-      .catch(error => {
-        console.error('Error al conectar con el servidor:', error);
-      });
-  }, []);
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} /> {/* Ruta principal para la vista de bienvenida */}
-
-      </Routes>
-    </Router>
+    <div>
+      <Welcome />
+    </div>
   );
 }
 
